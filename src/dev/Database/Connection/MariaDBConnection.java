05e6c70ea26189a0f01529f7e03e0a49660b147d
faysal <faysalkharraz@gmail.com>
2019-11-30 15:36:11 +0000

    "I have finished building the class needed to create a mariadb connection. Note: Functionality not tested yet! I am also constructing a flowchart for the program which can be found on draw.io"

--- src/dev/Database/Connection/MariaDBConnection.java
+++ src/dev/Database/Connection/MariaDBConnection.java
@@ -4,9 +4,43 @@ import java.sql.*;
 
 public class MariaDBConnection {
 
-    /* Defining needed attributes for setting up the connectio */
+    /* Defining needed attributes for setting up the connection */
+    private final String DB_URL = "jdbc:mariadb://fa32.host.cs.st-andrews.ac.uk/fa32_GUI";
     private final String USERNAME = "fa32";
+    private final String PASSWORD = "DF3600Dpx4.UcK";
 
-    //Trying to establish a connection with mariadb
+    /* Informing the user that connection is being established */
+    private final String CONNECTING_MESSAGE = "CONNECTING TO DATABASE";
+    private final String CONNECTION_ESTABLISHED_MESSAGE = "Connection established";
+
+    /* List of error messages to be printed to the user/system */
+    private final String SQL_EXCEPTION_ERROR_MESSAGE = "SQL Exception: ";
+    private final String MESSAGE_SEPARATOR = "---------------------------------------- \n";
+    private final String CONNECTION_ERROR_MESSAGE = "Connection Error: Failure to Connect to Database";
+
+    //****************************************************************************************************************//
+
+    public Connection establishConnection() {
+        Connection databaseConnection = null;
+
+        try {
+            System.out.println(CONNECTING_MESSAGE);
+
+            //Trying to establish a connection with mariadb
+            databaseConnection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+
+        } catch (SQLException e) {
+            //to be printed to system stderr
+            System.err.println(SQL_EXCEPTION_ERROR_MESSAGE + e.getMessage());
+            System.err.println(MESSAGE_SEPARATOR);
+
+            //to be printed to user
+            System.out.println(CONNECTION_ERROR_MESSAGE);
+        }
+
+        System.out.println(CONNECTION_ESTABLISHED_MESSAGE);
+        return databaseConnection;
+    }
+
 
 }
