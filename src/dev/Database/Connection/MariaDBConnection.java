13d8a949773caa0da1aeb3fd2057a61485ddc428
Faysal Alkharraz <faysal@8afbf9f1.st-andrews.ac.uk>
2019-11-30 13:10:29 +0000

    I have added a new package needed to setup the database connection

---
+++ src/dev/Database/Connection/MariaDBConnection.java
@@ -0,0 +1,12 @@
+package dev.Database.Connection;
+
+import java.sql.*;
+
+public class MariaDBConnection {
+
+    /* Defining needed attributes for setting up the connectio */
+    private final String USERNAME = "fa32";
+
+    //Trying to establish a connection with mariadb
+
+}
